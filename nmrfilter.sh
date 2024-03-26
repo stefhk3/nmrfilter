164996dc84352cffb1628493c1b7b2b571349eb0
kulakarla <karl@DESKTOP-H2KPROG>
2024-03-26 16:01:12 +0200

    Conda environment check before running

--- nmrfilter.sh
+++ nmrfilter.sh
@@ -3,28 +3,39 @@
 # Get the directory path where the shell script is located
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Check if the virtual environment already exists in the script's directory
-if [ -f "$script_dir/nmrfilter_env/bin/activate" ]; then
-    echo "NMRfilter_env already exists. Activating..."
-    source "$script_dir/nmrfilter_env/bin/activate"
+
+if [[ -z $CONDA_SHLVL || $CONDA_SHLVL == 0 ]]; then
+    echo "Conda environment is not in use, using Python Virtual Environment"
+    # Check if the virtual environment already exists in the script's directory
+    if [ -f "$script_dir/nmrfilter_env/bin/activate" ]; then
+        echo "NMRfilter_env already exists. Activating..."
+        source "$script_dir/nmrfilter_env/bin/activate"
+    else
+        echo "Creating virtual environment in $script_dir/nmrfilter_env..."
+        python3 -m venv "$script_dir/nmrfilter_env"
+        source "$script_dir/nmrfilter_env/bin/activate"
+
+        echo "Installing the requirements..."
+        python -m pip install -r requirements.txt
+
+        echo "Installing Jupyter Notebook..."
+        python -m pip install jupyter
+    fi
 else
-    echo "Creating virtual environment in $script_dir/nmrfilter_env..."
-    python3 -m venv "$script_dir/nmrfilter_env"
-    source "$script_dir/nmrfilter_env/bin/activate"
-
-    echo "Installing the requirements..."
-    python -m pip install -r requirements.txt
-
-    echo "Installing Jupyter Notebook..."
-    python -m pip install jupyter
+    echo "Using conda environment.."
 fi
+
 
 #start the processing
 python3 nmrfilter.py $1
 out=$(java -cp "./*" uk.ac.dmu.simulate.Convert $1) 
+echo "Simulate Convert has been called"
+
 #out=$(echo $out | tr -d '\n')
 readarray -d _ -t outs <<<"$out"
 outs[2]=$(echo ${outs[2]} | tr -d '\n')
+
+
 if [ ${outs[0]} = 'true' ]
 then
     cd respredict
